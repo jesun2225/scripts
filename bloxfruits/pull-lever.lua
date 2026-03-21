1b08792d4bf50f22d3bdb5f681cac34f7472ba54
Jesun Tolentino <98019179+jesun2225@users.noreply.github.com>
2026-03-21 17:18:48 +0600

    Update pull-lever.lua

--- bloxfruits/pull-lever.lua
+++ bloxfruits/pull-lever.lua
@@ -2,7 +2,7 @@ getgenv().Config = {
     Team = "Marnies", -- Pirates
     Settings = {
         ToolFarm = "Fruit", -- Melee
-        HopDelay = 2
+        HopDelay = 22
     }
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/WhiteX1208/Scripts/refs/heads/main/PullLever.luau"))()
