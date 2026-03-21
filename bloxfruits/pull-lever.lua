5b8a9b1ef8efba49d56d16c9c06d2bf5f91872a7
Jesun Tolentino <98019179+jesun2225@users.noreply.github.com>
2026-03-21 12:59:03 +0600

    Clarify ToolFarm setting comment in pull-lever.lua
    Updated ToolFarm setting comment to clarify its purpose.

---
+++ bloxfruits/pull-lever.lua
@@ -0,0 +1,8 @@
+getgenv().Config = {
+    Team = "Pirates", -- uh not work now
+    Settings = {
+        ToolFarm = "Fruit", -- Melee
+        HopDelay = 2
+    }
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/WhiteX1208/Scripts/refs/heads/main/PullLever.luau"))()
