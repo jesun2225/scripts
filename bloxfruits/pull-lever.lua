23091f6653eb99be1018ffb5240f641a17229ca8
Jesun Tolentino <98019179+jesun2225@users.noreply.github.com>
2026-03-21 17:22:08 +0600

    Update pull-lever.lua

--- bloxfruits/pull-lever.lua
+++ bloxfruits/pull-lever.lua
@@ -2,7 +2,7 @@ getgenv().Config = {
     Team = "Pirates", -- uh not work now
     Settings = {
         ToolFarm = "Melee",
-        HopDelay = 2
+        HopDelay = 22
     }
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/638cb8130072d62781731a34792acec1.lua"))()
