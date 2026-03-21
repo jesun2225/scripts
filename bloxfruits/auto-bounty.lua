b286ea28911e4b4553c1ad64b619a3afef558bc0
Jesun Tolentino <98019179+jesun2225@users.noreply.github.com>
2026-03-21 17:45:51 +0600

    Update auto-bounty.lua

--- bloxfruits/auto-bounty.lua
+++ bloxfruits/auto-bounty.lua
@@ -10,16 +10,18 @@ print("✅ Delay complete! Loading config & script NOW...")
 
 getgenv().Config = {
     Team = "Pirates",
+    HideUI = true,
     HuntConfig = {
         ["Earned Notification Enabled"] = true,
-        ["Farm Delay"] = 0.22,
+        ["Reset Farm (New)"] = true,
+        ["Chat"] = false,
+        ["Farm Delay"] = 0.22, -- 0.15 - Super Fast(Risk - Kick) | 0.22 Fast | 0.35 Medium | Max 0.5
         ["Webhook"] = {
-            Enabled = true,
+            Enabled = false,
             Url = "https://discord.com/api/webhooks/1483703956017512519/AiCEE2cTisDvnxoJnWIsGQZuWu-B_9uN7cPBHvOUnYdsc9iOVebrtFqcIaMNSoBcuWYY"
         }
     }
 }
-
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/2ffcdb62773f587bfb9eb0d52bb35b0c.lua"))()
 
 print("🎉 Script fully loaded & running! Happy farming bro! 💰")
